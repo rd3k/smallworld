cd2b76c4139019b24407e13deb45b76ea0965f89
rd3k <jamestherobertson@gmail.com>
2014-05-09 17:05:21 +0100

    improved file organisation

---
+++ js/all.js
@@ -0,0 +1,8 @@
+// @codekit-append "src/lib/viva.min.js"
+// @codekit-append "src/lib/permnowpolyfill.min.js"
+// @codekit-append "src/lib/rng.min.js"
+// @codekit-append "src/FullScreen.js"
+// @codekit-append "src/InfoBubble.js"
+// @codekit-append "src/Random.js"
+// @codekit-append "src/ShortestPathAlgo.js"
+// @codekit-append "src/SmallWorldAlgo.js"
--- js/src/ShortestPathAlgo.js
+++
@@ -1,1 +0,0 @@
-var App;!function(a){var l;!function(a){a[a.Dijkstra=0]="Dijkstra",a[a.BFS=1]="BFS",a[a.FloydWarshall=2]="FloydWarshall"}(l||(l={}))}(App||(App={}));
